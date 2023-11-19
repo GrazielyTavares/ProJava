97aad91f57f3f801b6c2c6953d5160e0a84005b8
Graziely Tavares <grazitds2005@gmail.com>
2023-11-19 15:38:40 -0300

    Create Notas.java

---
+++ Orientacao/src/Ent/Notas.java
@@ -0,0 +1,13 @@
+package Ent;
+
+public class Notas {
+
+    public String nome;
+
+    public double not1, not2, not3;
+
+    public double Calculo(){
+
+        return not1 + not2 + not3;
+    }
+}

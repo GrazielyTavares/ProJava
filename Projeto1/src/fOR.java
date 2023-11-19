51fe258fd567b6f31934c3053ebb71717e17c76e
Graziely Tavares <grazitds2005@gmail.com>
2023-11-19 15:37:27 -0300

    Create fOR.java

---
+++ Projeto1/src/fOR.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class fOR {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+//        for (int i = 1; i <= x; i++) {
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+        //TESTE 2
+//        int n = sc.nextInt();
+//
+//        for (int i=0; i<n; i++) {
+//
+//            double a = sc.nextDouble();
+//            double b = sc.nextDouble();
+//            double c = sc.nextDouble();
+//
+//            double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+//
+//            System.out.printf("%.1f%n", media);
+//        }
+//
+//        sc.close();
+    }
+}
+

9f3e842270ffad467c9821d6e468d9550aafba42
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-11-22 10:48:03 -0300

    Create pensionato.java

---
+++ principal/pensionato.java
@@ -0,0 +1,18 @@
+package principal;
+
+import comp.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class pensionato {
+
+    Locale.setDefault(Locale.US);
+    Scanner ab = new Scanner(System.in);
+
+    int an = ab.nextInt();
+
+    //linha que referência a variavel, como vetor no objeto
+    Product[] vect = new Product[an];
+
+}

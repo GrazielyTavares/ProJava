8c7b22353c62c329b82d6f539806f9c1d85b181b
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-11-22 10:47:57 -0300

    Create pens.java

---
+++ comp/pens.java
@@ -0,0 +1,28 @@
+package comp;
+
+public class pens {
+
+    private String name;
+    private String email;
+
+    private int rom;
+
+    public InfoDormi(String name, String email, int rom){
+        this.name = name;
+        this.email = email;
+        this.rom = rom;
+    }
+
+    public String getName(){ return name; }
+
+    public void setName(String name){ this.name = name; }
+
+    public String getEmail(){ return email; }
+
+    public void setEmail(String email){ this.email = email; }
+
+    public int getRom(){ return rom; }
+
+    public void setRom(int rom){ this.rom = rom; }
+
+}

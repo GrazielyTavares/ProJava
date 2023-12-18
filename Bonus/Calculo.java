ea3fd6d2c1aa316166dbb852e8f5526f219bd1af
Graziely Tavares <grazitds2005@gmail.com>
2023-12-17 23:09:21 -0300

    Create Calculo.java

---
+++ Bonus/Calculo.java
@@ -0,0 +1,19 @@
+package Bonus;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Calculo {
+    
+    // data-hora para Texto
+    LocalDate d04 = LocalDate.parse("2023-12-17");
+    LocalDateTime d05 = LocalDateTime.parse("2023-12-17T22:06:45");
+    Instant d06 = Instant.parse("2023-12-17T22:07:15Z");
+
+    //diminuir os dias
+    LocalDate pastWeeLocalDate = d05.minusDays(7);
+    //adicionar os dias
+    LocalDate nextWeeLocalDate = d05.plusDays(7);
+
+}

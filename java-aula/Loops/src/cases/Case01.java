1b81d84db293d75506e927f38fb4f4688354ff1e
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2024-05-15 22:12:36 -0300

    Create Case01.java

---
+++ java-aula/Loops/src/cases/Case01.java
@@ -0,0 +1,23 @@
+package cases;
+
+import java.util.Scanner;
+
+public class Case01{
+
+    public static void main(String[]args){
+
+        Scanner ab = new Scanner(System.in);
+        String a;
+
+        System.out.println("Informe uma vogal: ");
+        a = ab.next();
+
+        a = a.toLowerCase();
+
+        if (a.equals('a') || a.equals('e') || a.equals('i') || a.equals('o') || a.equals('u')){
+            System.out.println("A letra é uma vogal");
+        }else{
+            System.out.println("A letra não é uma vogal");
+        }
+    }
+}

837868bcda8200a66cb4fa969c3e40dc87e45ac5
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2024-05-15 22:12:31 -0300

    Create Case03.java

---
+++ java-aula/Loops/src/cases/Case03.java
@@ -0,0 +1,28 @@
+package cases;
+import java.util.Scanner;
+
+public class Case03{
+
+    public static void main(String[] args){
+
+        Scanner ab = new Scanner(System.in);
+        int a, b;
+
+        System.out.println("Informe o numero: ");
+      
+        a = ab.nextInt();
+
+        //Diminuindo a entrada para minuscula
+        b = a%2;
+
+
+        switch(b){
+            case 0:
+                System.out.println("Numero é par");
+                break;
+            default:
+                System.out.println("Numero é impar");
+        }
+       
+    }
+}

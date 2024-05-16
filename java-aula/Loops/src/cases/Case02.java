4eaeecf88c76b652626d21c9d5c45e4aa37e6a84
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2024-05-15 22:12:34 -0300

    Create Case02.java

---
+++ java-aula/Loops/src/cases/Case02.java
@@ -0,0 +1,32 @@
+package cases;
+import java.util.Scanner;
+
+public class Case02{
+
+    public static void main(String[] args){
+
+        Scanner ab = new Scanner(System.in);
+        String a;
+
+        System.out.println("Informe uma vogal: ");
+      
+        a = ab.next();
+
+        //Diminuindo a entrada para minuscula
+        a.toLowerCase();
+
+
+        switch(a){
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+                System.out.println("A letra é uma vogal");
+                break;
+            default:
+                System.out.println("A letra não é uma vogal");
+        }
+       
+    }
+}
